8d502b52038b6d2aeacab2ab213a67f2ec22c226
Keeler Spear <keelerspear2@gmail.com>
2025-02-25 16:47:00 -0700

    Implemented a Predictor-Corrector 4 method

--- src/Workspace.java
+++ src/Workspace.java
@@ -1,4 +1,28 @@
 //A class for me to double-check my answers to written homework problems
 
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
 public class Workspace {
+    public static void main(String[] args) {
+        double h = 0.125;
+        double t0 = 1.0;
+        double y0 = 2.0;
+        double tMax = 2.0;
+
+
+        BiFunction<Double, Double, Double> diffEq = (t, y) -> 1.0 + y / t;
+        Function<Double, Double> fnc = (t) -> t * Math.log(t) + 2 * t;
+
+        double[] sol = ODE.adamsBash(diffEq, t0, y0, tMax, h);
+        double[] pcSol = ODE.pc(diffEq, t0, fnc.apply(t0), tMax, h);
+
+        double exact = fnc.apply(tMax);
+        double adamAp = sol[sol.length - 1];
+        double rk4Ap = pcSol[pcSol.length - 1];
+
+        System.out.println("Exact " + exact);
+        System.out.println("Adam's Approx: " + adamAp + " | Error:" + Error.absolute(exact, adamAp));
+        System.out.println("P-C Approx: " + rk4Ap + " | Error:" + Error.absolute(exact, rk4Ap));
+    }
 }
