34b4cf992889d7a9e8cc9cbeb2b13832d7c0565e
Keeler Spear <keelerspear2@gmail.com>
2025-02-25 20:55:27 -0700

    Commit

---
+++ src/PA2.java
@@ -0,0 +1,33 @@
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+public class PA2 {
+
+    public static void main(String[] args) {
+        double h = 0.5;
+        double t0 = 1.0;
+        double y0 = 2.0;
+        double tMax = 100.0;
+
+
+        BiFunction<Double, Double, Double> diffEq = (t, y) -> 1.0 + y / t;
+        Function<Double, Double> fnc = (t) -> t * Math.log(t) + 2 * t;
+
+        double[] sol = ODE.adamsBash(diffEq, t0, y0, tMax, h);
+        double[] pcSol = ODE.pc(diffEq, t0, fnc.apply(t0), tMax, h);
+
+        double exact = fnc.apply(tMax);
+        double adamAp = sol[sol.length - 1];
+        double rk4Ap = pcSol[pcSol.length - 1];
+
+        Matrix x = LinearAlgebra.linSpace(t0, tMax, h);
+        Matrix real = LinearAlgebra.applyFunction(x, fnc);
+        Matrix approx = new Matrix(sol);
+        Matrix pcApprox = new Matrix(pcSol);
+
+        System.out.println("Exact " + exact);
+        System.out.println("Adam's Approx: " + adamAp + " | Error:" + Error.absolute(exact, adamAp));
+        System.out.println("P-C Approx: " + rk4Ap + " | Error:" + Error.absolute(exact, rk4Ap));
+    }
+
+}
--- src/Workspace.java
+++ src/Workspace.java
@@ -5,29 +5,22 @@ import java.util.function.Function;
 
 public class Workspace {
     public static void main(String[] args) {
-        double h = 0.5;
-        double t0 = 1.0;
-        double y0 = 2.0;
-        double tMax = 100.0;
+        double l = 3;
+        int lNum = 5;
+        int n = 100;
 
+        Function<Double, Double> f = x -> x;
 
-        BiFunction<Double, Double, Double> diffEq = (t, y) -> 1.0 + y / t;
-        Function<Double, Double> fnc = (t) -> t * Math.log(t) + 2 * t;
+        Matrix xLinSpace = LinearAlgebra.linSpace(-l * lNum, l * lNum, 0.1);
 
-        double[] sol = ODE.adamsBash(diffEq, t0, y0, tMax, h);
-        double[] pcSol = ODE.pc(diffEq, t0, fnc.apply(t0), tMax, h);
+        Matrix lin = LinearAlgebra.applyFunction(xLinSpace, f);
 
-        double exact = fnc.apply(tMax);
-        double adamAp = sol[sol.length - 1];
-        double rk4Ap = pcSol[pcSol.length - 1];
+        Matrix fourier = PDE.fourierSeries(f, l, n);
 
-        Matrix x = LinearAlgebra.linSpace(t0, tMax, h);
-        Matrix real = LinearAlgebra.applyFunction(x, fnc);
-        Matrix approx = new Matrix(sol);
-        Matrix pcApprox = new Matrix(pcSol);
+        fourier = LinReg.buildFunction(xLinSpace, fourier, BasisFunctions.fourier(n, l));
 
-        System.out.println("Exact " + exact);
-        System.out.println("Adam's Approx: " + adamAp + " | Error:" + Error.absolute(exact, adamAp));
-        System.out.println("P-C Approx: " + rk4Ap + " | Error:" + Error.absolute(exact, rk4Ap));
+        String title = "Fourier Series of Order " + String.valueOf(n);
+        PyChart.twoFnc(xLinSpace, lin, "Y=x", fourier, "Fourier Series", "X", "Y", title);
+
     }
 }
